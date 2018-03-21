741038d48bd63e596db11f0753d7cfa7aea10658
naveeneaswar <37346618+naveeneaswar@users.noreply.github.com>
2018-03-21 10:51:58 +0530

    Update primeornot.py

--- beginner level/primeornot.py
+++ beginner level/primeornot.py
@@ -1,9 +1,15 @@
+def main():
+    pass
 a=int(input("Enter number: "))
-sum=0
-for i in range(2,a//2+1):
-    if(a%i==0):
-        sum=sum+1
-if(sum<=0):
-    print("yes")
+if(a>1):
+    for i in range(2,a):
+        if(a%i==0):
+            print("no")
+            break
+    else:
+            print("yes")
 else:
-    print("no")
+    print("not a prime")
+
+if __name__ == '__main__':
+    main()
