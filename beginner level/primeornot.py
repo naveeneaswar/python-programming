944b34680b449a3eeefe3cb8f4e3c5d4ed48fe8f
naveeneaswar <37346618+naveeneaswar@users.noreply.github.com>
2018-03-21 09:46:04 +0530

    Update primeornot.py

--- beginner level/primeornot.py
+++ beginner level/primeornot.py
@@ -1,9 +1,9 @@
 a=int(input("Enter number: "))
-k=0
+sum=0
 for i in range(2,a//2+1):
     if(a%i==0):
-        k=k+1
-if(k<=0):
+        sum=sum+1
+if(sum<=0):
     print("yes")
 else:
     print("no")
